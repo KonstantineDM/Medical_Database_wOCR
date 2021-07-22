879e163dc0e2a1cb1c83be0210e57084f342db87
KonstantineDM <iteamallard@gmail.com>
2021-07-22 21:23:19 +0300

    File for font styles

---
+++ fonts_db.py
@@ -0,0 +1,5 @@
+import tkinter.font as tkfont
+
+def font_large(root): return tkfont.Font(root, family='Times',size=30)
+def font_medium(root): return tkfont.Font(root, family='Times',size=20)
+def font_small(root): return tkfont.Font(root, family='Times',size=14)
